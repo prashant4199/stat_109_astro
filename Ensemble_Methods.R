5dad47bee16e551fd7775091e4360e819ef0a290
mikeAtHarvard <103788697+mikeAtHarvard@users.noreply.github.com>
2022-05-04 21:17:35 -0500

    Update Ensemble_Methods.R

--- Ensemble_Methods.R
+++ Ensemble_Methods.R
@@ -6,20 +6,21 @@ library(caret)
 library(e1071)
 library(lime)
 library(psych)
+library(rpart)
+library(rpart.plot)
+library(pROC)
+library(tree)
+
 options(scipen=999)
 
 df <- read.csv(file = "DataStars1.csv", header = T)
 df$Type <- as.factor(df$Type) # convert the variable Type as a factor.
 
-
-str(df)
-summary(df)
 pairs.panels(df, ellipses=F, bg = c("red","green","yellow","blue","lightgreen","black")[df$Type], hist.col = "gray"
              , lm = F)
 
 table(df$Type)
 set.seed(1234)
-
 ind <- sample(2, nrow(df), replace = T, prob = c(0.75, 0.25))
 train <- df[ind == 1,]
 test <- df[ind == 2,]
@@ -27,27 +28,29 @@ test <- df[ind == 2,]
 
 #Bagging
 
-set.seed(1234)
+
 cvcontrol <- trainControl(method = "repeatedcv",
                           number = 5,
                           repeats = 2,
                           allowParallel = T
 )
 
-set.seed(1234)
+
 bag <- train(Type ~ .,
              data = train,
              method = "treebag",
              trControl = cvcontrol,
              importance = T)
 plot(varImp(bag), main = "BAGGING")
+
+p_train <- predict(bag, train, type = 'raw')
+confusionMatrix(p_train, train$Type)
+
 p <- predict(bag, test, type = 'raw')
 confusionMatrix(p, test$Type)
 
 
 #RF
-
-set.seed(1234)
 forest <- train(Type ~ .,
                 data = train,
                 method = "rf",
@@ -55,6 +58,10 @@ forest <- train(Type ~ .,
                 importance = T)
 plot(forest)
 plot(varImp(forest), main = "RANDOM FOREST")
+
+p_train <- predict(forest, train, type = 'raw')
+confusionMatrix(p_train, train$Type)
+
 p <- predict(forest, test, type = 'raw')
 confusionMatrix(p, test$Type)
 
@@ -85,6 +92,10 @@ boo <- train(Type ~ .,
                                      subsample =1))
 
 plot(varImp(boo), main = "XG BOOST Gamma = 1")
+
+p_train <- predict(boo, train, type = 'raw')
+confusionMatrix(p_train,train$Type)
+
 p <- predict(boo, test, type = 'raw')
 confusionMatrix(p,test$Type)
 
@@ -138,13 +149,3 @@ plot(varImp(boo), main = "XG BOOST Gamma = 0")
 p <- predict(boo, test, type = 'raw')
 confusionMatrix(p,test$Type)
 
-
-
-# Temperature
-# Luminosity
-# Radius
-# A_M Absolute Magnitude
-# Color
-# Spectral Class
-# Type
-# HR Diagram Hertzsprung Russell Diagram
